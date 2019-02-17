cc8fe621b68a7170d0e8154bc2d79ea4cd1ead35
viethoang92 <vi_hoang@hotmail.de>
2019-02-17 19:29:20 +0100

    Updated ready event

--- client/events/ready.js
+++ client/events/ready.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 /**
  * This event will run if the bot starts, and logs in, successfully.
  */
@@ -5,4 +6,12 @@ module.exports = async (client) => {
     await console.log(`${client.user.username} has started with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
     await client.user.setActivity(process.env.activity).catch(console.error);
     client.user.setStatus(process.env.status).catch(console.error);
+    let embed = await new Discord.RichEmbed()
+        .setColor("GREEN")
+        .setDescription(`Ready`)
+        .setTimestamp();
+    let channel = client.channels.get(process.env.CONSOLELOGID);
+    if(channel && channel.permissionsFor(channel.guild.me).has("SEND_MESSAGES")){
+        channel.send(embed);
+    }
 };
