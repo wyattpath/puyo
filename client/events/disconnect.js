22b7da8ee99d1dbda8fd4e76502035875851a9ab
viethoang92 <vi_hoang@hotmail.de>
2019-02-19 19:56:27 +0100

    Added disconnect event

---
+++ client/events/disconnect.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+/**
+ * This event will run if the bot fails to reconnect and stays disconnected
+ */
+module.exports = async (client) => {
+
+    let embed = await new Discord.RichEmbed()
+        .setColor("RED")
+        .setDescription(`Disconnected!`);
+    let channel = await client.channels.get(process.env.CONSOLELOGID);
+    if(channel && channel.permissionsFor(channel.guild.me).has("SEND_MESSAGES")){
+        channel.send(embed);
+    }
+    console.log("Disconnected");
+};
