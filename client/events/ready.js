e277b5fa252871df334425a73f0472947962453a
viethoang92 <vi_hoang@hotmail.de>
2019-04-12 04:44:39 +0200

    Updated ready event

--- client/events/ready.js
+++ client/events/ready.js
@@ -1,16 +1,33 @@
 const Discord = require('discord.js');
+const {Tags} = require('../dbObjects');
+const {Servers} = require('../dbObjects');
+
 /**
  * This event will run if the bot starts, and logs in, successfully.
  */
 module.exports = async (client) => {
-    await console.log(`${client.user.username} has started with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
     await client.user.setActivity(process.env.activity);
     await client.user.setStatus(process.env.status);
+    await Tags.sync();
+    await Servers.sync();
+    const guildArray = await client.guilds.array();
+    let guild, i;
+    for (i = 0; i < guildArray.length; i++) {
+        guild = await guildArray[i];
+        let server = await Servers.findOne({where: {server_id: guild.id}});
+        if(!server) {
+            console.log(i);
+            await Servers.create({server_id: guild.id});
+        }
+        console.log(" - " + guild.name);
+    }
+    await console.log(`${client.user.username} has started with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+
     let embed = await new Discord.RichEmbed()
         .setColor("GREEN")
         .setDescription(`Ready`)
         .setTimestamp();
-    let channel = await client.channels.get(process.env.CONSOLELOGID);
+    let channel = await client.channels.get(process.env.LOGCHANNELID);
     if (!channel || !channel.permissionsFor(channel.guild.me).has("SEND_MESSAGES")) return;
     await channel.send(embed);
 };
