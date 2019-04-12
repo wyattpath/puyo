93d82dc48261e7245f22457cfdd11341ed0ade8d
viethoang92 <vi_hoang@hotmail.de>
2019-04-12 04:59:24 +0200

    Added settings command

---
+++ client/commands/settings.js
@@ -0,0 +1,28 @@
+const {Servers} = require('../dbObjects');
+const {RichEmbed} = require('discord.js');
+/**
+ * Shows server settings
+ */
+module.exports.run = async (client, msg) => {
+    if (!msg.channel.permissionsFor(msg.guild.me).has("SEND_MESSAGES")) return;
+    if (!msg.member.hasPermission('MANAGE_GUILD')) return;
+
+    // equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
+    const server = await Servers.findOne({where: {server_id: msg.guild.id}});
+    let modRole = await msg.guild.roles.get(server.get('modRole_id')) || "Empty";
+    let modLog = await msg.guild.channels.get(server.get('modLog_id')) || "Empty";
+    let sEmbed = new RichEmbed()
+        .setTitle('Server Settings')
+        .setColor("AQUA")
+        .addField('Prefix', server.get('prefix'))
+        .addField('Mod Role', modRole)
+        .addField('Mod Log', modLog);
+    return msg.channel.send(sEmbed);
+};
+
+module.exports.help = {
+    name: "settings",
+    description: "shows server settings",
+    usage: "settings",
+    category: "admin"
+};
