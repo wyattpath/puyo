e00eb6471fa69afbd5fa40d98db6f13d0d55aea8
viethoang92 <vi_hoang@hotmail.de>
2019-04-16 11:07:12 +0200

    Updated guildCreate event - more info

--- client/events/guildCreate.js
+++ client/events/guildCreate.js
@@ -8,6 +8,17 @@ module.exports = async (client, guild) => {
     try {
         // equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
         await Servers.create({server_id: guild.id});
+        let embed = await new Discord.RichEmbed()
+            .setColor("GREEN")
+            .setTitle(`Server added`)
+            .setDescription(`Puyo added to Server ${guild.name} ! Puyo is now on ${client.guilds.size} servers!`)
+            .addField(`Owner`, guild.owner)
+            .addField(`Server ID`, guild.id)
+            .addField(`Server Size`, guild.memberCount)
+            .setTimestamp();
+        let channel = await client.channels.get(process.env.CONSOLELOGID);
+        if (!channel || !channel.permissionsFor(channel.guild.me).has("SEND_MESSAGES")) return;
+        await channel.send(embed);
     } catch (e) {
         if (e.name === 'SequelizeUniqueConstraintError') {
             return console.log('That server already exists.');
