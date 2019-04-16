0ddf9e8a0b6100435ddfb5126d201e4771a17721
viethoang92 <vi_hoang@hotmail.de>
2019-04-16 11:06:56 +0200

    Updated guildDelete event - more info

--- client/events/guildDelete.js
+++ client/events/guildDelete.js
@@ -7,7 +7,20 @@ module.exports = async (client, guild) => {
     await Servers.sync();
     try {
         // equivalent to: DELETE from tags WHERE name = ?;
-        await Servers.destroy({where: {server_id: guild.id}});
+        const rowCount = await Servers.destroy({where: {server_id: guild.id}});
+        if (!rowCount) return msg.reply('That tag did not exist.');
+        let embed = await new Discord.RichEmbed()
+            .setColor("RED")
+            .setTitle(`Server removed`)
+            .setDescription(`Puyo removed from Server ${guild.name} ! Puyo is now on ${client.guilds.size} servers!`)
+            .addField(`Owner`, guild.owner)
+            .addField(`Server ID`, guild.id)
+            .addField(`Server Size`, guild.memberCount)
+            .setTimestamp();
+        let channel = await client.channels.get(process.env.CONSOLELOGID);
+        if (channel && channel.permissionsFor(channel.guild.me).has("SEND_MESSAGES")) {
+            return channel.send(embed);
+        }
     } catch (e) {
         if (e.name === 'SequelizeUniqueConstraintError') {
             return console.log('That server is already deleted.');
